9e12368de4c2c2f1bc574556823f3d63e756db3e
Dmitriy Guseinov <Diman777666@yandex.ru>
2023-07-20 12:06:39 +0400

    Auto-generated commit

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  clientId: '',
-  clientSecret: '',
-  threadsCount: 3,
-  baseURL: 'https://api.aspose.cloud/v3.0/imaging/convert',
-  authURL: 'https://api.aspose.cloud/connect/token',
-  uploadMaxLength: 30,
-};
